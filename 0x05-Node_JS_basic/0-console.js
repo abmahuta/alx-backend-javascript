1ecdd60dd6b7b3be249dccf2f56a93c1b89cd76d
Abdulkadir <mahuta2012@gmail.com>
2023-05-16 09:59:09 +0100

    Task 0

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,10 @@
+/**
+ * Prints a strig to the STDOUT.
+ * @param {String} msg The string to display.
+ * @author Abdulkadir Badamasi <https://github.com/abmahuta>
+ */
+const displayMessage = (msg) => {
+  console.log(msg);
+};
+
+module.exports = displayMessage;
